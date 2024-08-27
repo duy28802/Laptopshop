0ec3a46f855ebdccc3dc634c3aa576ea568f555d
duy <lethanhduy28802@gmail.com>
2024-08-27 13:45:37 +0700

    update relationship

--- src/main/java/vn/hoidanit/laptopshop/model/User.java
+++ src/main/java/vn/hoidanit/laptopshop/model/User.java
@@ -1,12 +1,18 @@
 package vn.hoidanit.laptopshop.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.OneToMany;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -18,6 +24,17 @@ public class User {
     private String fullName;
     private String address;
     private String phoneNumber;
+
+    private String avatar;
+
+    // roleId
+    // User many to one role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    List<Order> orders;
 
     public void setId(long id) {
         this.id = id;
@@ -67,10 +84,22 @@ public class User {
         return phoneNumber;
     }
 
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phoneNumber=" + phoneNumber + "]";
+                + ", address=" + address + ", phoneNumber=" + phoneNumber + ", avatar=" + avatar + "]";
     }
 
 }
