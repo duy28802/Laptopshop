5cf6631233779b777282e5d220f82449155fa12b
duy <lethanhduy28802@gmail.com>
2024-12-02 09:49:38 +0700

    a

--- src/main/java/vn/duy/laptopshop/repository/CartDetailRepository.java
+++ src/main/java/vn/duy/laptopshop/repository/CartDetailRepository.java
@@ -3,9 +3,13 @@ package vn.duy.laptopshop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import vn.duy.laptopshop.domain.Cart;
 import vn.duy.laptopshop.domain.CartDetail;
+import vn.duy.laptopshop.domain.Product;
 
 @Repository
 public interface CartDetailRepository extends JpaRepository<CartDetail, Long> {
+    boolean existsByCartAndProduct(Cart cart, Product product);
 
+    CartDetail findByCartAndProduct(Cart cart, Product product);
 }
